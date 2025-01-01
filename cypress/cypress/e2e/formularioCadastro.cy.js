e7f5162151eecf0b586cd1badf9e5843cd04f70d
Amanda92Cortez <amanda_cortez_2012@hotmail.com>
2025-01-01 16:52:44 -0300

    Cypress

--- cypress/cypress/e2e/formularioCadastro.cy.js
+++ cypress/cypress/e2e/formularioCadastro.cy.js
@@ -5,10 +5,10 @@ describe('Formulário Cadastro', ()=>{
 
   it('Usuário deve conseguir se cadastrar com sucesso', ()=>{
     cy.getByData('botao-cadastro').click()
-    cy.getByData('nome-input').type('Teste Alura')
-    cy.getByData('email-input').type('teste@email.com')
-    cy.getByData('senha-input').type('1234')
+    cy.getByData('nome-input').type('Gui Lima')
+    cy.getByData('email-input').type('gui@email.com')
+    cy.getByData('senha-input').type('456789')
     cy.getByData('botao-enviar').click()
-    // cy.getByData('mensagem-sucesso').should('exist').and('have.text', 'Usuário cadastrado com sucesso!')
+    cy.getByData('mensagem-sucesso').should('exist').and('have.text', 'Usuário cadastrado com sucesso!')
   })
 })
