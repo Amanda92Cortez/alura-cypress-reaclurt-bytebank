25a6f8a1b63c0f31638c71fb72d8ad88214aa96b
Amanda92Cortez <amanda_cortez_2012@hotmail.com>
2025-01-05 17:19:03 -0300

    Cypress

---
+++ cypress/cypress/e2e/paginas-view.cy.js
@@ -0,0 +1,20 @@
+import {isMobile} from '../support/util'
+describe('Testando múltiplas páginas', () => { 
+  it('Deve conseguir acessar a página de cartões', ()=>{
+    cy.login(Cypress.env('email'), Cypress.env('senha'))
+    cy.visit('/home')
+    cy.location('pathname').should('eq','/home')
+
+    if(isMobile()){
+      cy.getByData('menu-burguer').should('be.visible')
+      cy.getByData('menu-burguer').click()
+      cy.getByData('app-home').find('a').eq(2).click()
+    } else {
+      cy.getByData('app-home').find('a').eq(2).click()
+    }
+
+    cy.getByData('titulo-cartoes').should('exist').and('have.text', 'Meus cartões')
+
+    cy.location('pathname').should('eq', '/home/cartoes')
+  })
+ })
